1660c078ee2ec90c505faf4e062f2fad3f5165f0
phatdroid94 <phatdroid94@gmail.com>
2020-01-02 18:49:07 +0700

    callback

--- app/src/main/java/com/example/progressbarseekbar26112019/MainActivity.java
+++ app/src/main/java/com/example/progressbarseekbar26112019/MainActivity.java
@@ -4,18 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    ProgressBar mProgressBar;
+//    ProgressBar mProgressBar;
+    SeekBar mSb,mSb2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mProgressBar = findViewById(R.id.progressbar);
+        mSb = findViewById(R.id.seekbar);
+        mSb2 = findViewById(R.id.seekbar2);
+//        mProgressBar = findViewById(R.id.progressbar);
         // Demo 1 : Loading view
 //        CountDownTimer countDownTimer = new CountDownTimer(3000,3000) {
 //            @Override
@@ -30,17 +36,40 @@ public class MainActivity extends AppCompatActivity {
 //        };
 //        countDownTimer.start();
         // Demo2 : Down load
-        CountDownTimer countDownTimer = new CountDownTimer(10000, 1000) {
+//        CountDownTimer countDownTimer = new CountDownTimer(10000, 1000) {
+//            @Override
+//            public void onTick(long millisUntilFinished) {
+//                mProgressBar.setProgress(mProgressBar.getProgress() + 10);
+//            }
+//
+//            @Override
+//            public void onFinish() {
+//                mProgressBar.setProgress(mProgressBar.getProgress() + 10);
+//            }
+//        };
+//        countDownTimer.start();
+        mSb.setOnSeekBarChangeListener(onSeekbarchange());
+        mSb2.setOnSeekBarChangeListener(onSeekbarchange());
+    }
+    private OnSeekBarChangeListener onSeekbarchange(){
+        return new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onTick(long millisUntilFinished) {
-                mProgressBar.setProgress(mProgressBar.getProgress() + 10);
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (mSb2.getProgress() >= mSb.getProgress()){
+                    mSb2.setProgress(mSb.getProgress());
+                }
             }
 
             @Override
-            public void onFinish() {
-                mProgressBar.setProgress(mProgressBar.getProgress() + 10);
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         };
-        countDownTimer.start();
     }
+
 }
